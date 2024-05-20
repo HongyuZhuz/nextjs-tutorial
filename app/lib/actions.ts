0af1f77bf6891dbf30f9a9d75908f5eb7b8c3fe9
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-05-20 13:16:40 +1000

    introduce zod library to make sure the type validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,10 +1,21 @@
 'use server'
+import {z} from 'zod';
+
+const FormSchema = z.object(
+    {
+        id:z.string(),
+        customerId:z.string(),
+        amount:z.coerce.number(),
+        status:z.enum(['pending','paid']),
+        date:z.string(),
+    }
+)
+const CreateInvice = FormSchema.omit({id:true,date:true});
 
 export async function createInvoice (formData: FormData){
-    const rawFormData = {
+    const {customerId, amount, status } = CreateInvice.parse({
         customerId: formData.get('customerId'),
         amount : formData.get('amount'),
         status: formData.get('status'),
-    };
-    console.log(rawFormData);
+    });
 }
